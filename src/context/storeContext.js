f3524f9a9078142ebc1697e2a984bc3b2502b121
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-15 23:10:35 +0100

    [NKL-4] Add useLocalStorage to useState with localStorage

--- src/context/storeContext.js
+++ src/context/storeContext.js
@@ -1,9 +1,10 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
+import { useLocalStorage } from '../Hooks/useLocalStorage';
 
 export const StoreContext = createContext();
 
 const StoreContextProvider = ({ children }) => {
-  const [goals, setGoals] = useState();
+  const [goals, setGoals] = useLocalStorage('goalsData', []);
 
   const store = {
     goalsState: [goals, setGoals],
