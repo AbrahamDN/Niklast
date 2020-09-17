357011be512e312ba029d9c2959eeecf36212a9c
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-17 03:38:48 +0100

    [NKL-13] Added max prop & some console.error 's

--- src/components/ListSteps/listSteps.component.jsx
+++ src/components/ListSteps/listSteps.component.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-function ListSteps({ steps, ...otherProps }) {
-  return steps.map((step, key) => (
-    <li {...otherProps} key={key}>
-      {step}
-    </li>
-  ));
+function ListSteps({ steps, max, ...otherProps }) {
+  if (!(typeof max === 'number'))
+    console.error(`ListSteps:`, ` typeof 'max' prop must be a number`);
+
+  steps = steps && max && typeof max === 'number' ? steps.slice(0, max) : steps;
+  return steps ? (
+    steps.map((step, key) => (
+      <li {...otherProps} key={key}>
+        {step}
+      </li>
+    ))
+  ) : (
+    <div>
+      {console.error(
+        `ListSteps: 'steps' prop is invalid`,
+        `Either the prop is missing or the data cannot be mapped`
+      )}
+    </div>
+  );
 }
 
 export default ListSteps;
