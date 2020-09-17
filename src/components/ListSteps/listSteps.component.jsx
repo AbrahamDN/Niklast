bc7ca35bef82bb19a513703cf5653963999c484f
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-17 15:00:04 +0100

    [NKL-13] Updated ListSteps console statements

--- src/components/ListSteps/listSteps.component.jsx
+++ src/components/ListSteps/listSteps.component.jsx
@@ -3,11 +3,23 @@ import GoalStep from '../GoalStep/goalStep.component';
 
 function ListSteps({ steps, max, goal, valueOnly, ...otherProps }) {
   if (max && !(typeof max === 'number'))
-    console.error(`ListSteps:`, ` typeof 'max' prop must be a number`);
+    console.warn(`ListSteps:`, ` typeof 'max' prop must be a number`);
+
+  if (!steps) {
+    return (
+      <span>
+        {console.warn(
+          `ListSteps: 'steps' prop is ${steps}`,
+          `Either the prop is missing or the data cannot be maped`
+        )}
+      </span>
+    );
+  }
 
   steps = steps && max && typeof max === 'number' ? steps.slice(0, max) : steps;
-  return steps ? (
-    steps.map((step, key) => (
+
+  return steps.map((step, key) => {
+    return (
       <li {...otherProps} key={key}>
         {valueOnly ? (
           <GoalStep step={step} valueOnly />
@@ -15,15 +27,8 @@ function ListSteps({ steps, max, goal, valueOnly, ...otherProps }) {
           <GoalStep step={step} goal={goal ? goal : null} />
         )}
       </li>
-    ))
-  ) : (
-    <div>
-      {console.error(
-        `ListSteps: 'steps' prop is invalid`,
-        `Either the prop is missing or the data cannot be mapped`
-      )}
-    </div>
-  );
+    );
+  });
 }
 
 export default ListSteps;
