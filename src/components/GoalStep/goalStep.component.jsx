1a31c418ca1330d7eb55cae8dc0965b033c800be
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-17 10:45:22 +0100

    [NKL-16] Added GoalStep

---
+++ src/components/GoalStep/goalStep.component.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StoreContext } from '../../context/storeContext';
+
+// import ListSteps from '../ListSteps/listSteps.component';
+
+function GoalStep({ step, goal, valueOnly, ...otherProps }) {
+  const {
+    goalsState: [goals, setGoals],
+  } = React.useContext(StoreContext);
+
+  const isComplete = event => {
+    const complete = event.target.checked;
+    return setGoals(
+      goals.map(obj =>
+        obj.id === goal.id
+          ? {
+              ...obj,
+              steps: obj.steps.map(mapStep =>
+                mapStep === step ? { ...mapStep, complete: complete } : mapStep
+              ),
+            }
+          : obj
+      )
+    );
+  };
+
+  if (valueOnly) {
+    return <span {...otherProps}>{step}</span>;
+  }
+
+  return (
+    <div {...otherProps}>
+      {goal ? (
+        <cite>
+          goal: <b>{goal.goal}</b>
+        </cite>
+      ) : null}
+      <div>{step.value}</div>
+      {step.complete ? 'completed' : null}
+      <input
+        type='checkbox'
+        defaultChecked={step.complete || false}
+        onChange={e => isComplete(e)}
+      />
+    </div>
+  );
+}
+
+export default GoalStep;
