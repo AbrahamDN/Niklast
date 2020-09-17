b85c9dc401ecab764f20a9cdf4fdb636774eb1e0
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-17 02:07:09 +0100

    Added InsertGoalStepsPage to sequence

--- src/pages/MainPage/mainPage.page.jsx
+++ src/pages/MainPage/mainPage.page.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 
 import Sequence from '../../components/Sequence/sequence.component';
 import InsertGoal from '../../components/InsertGoal/insertGoal.component';
+import InsertGoalStepsPage from '../InsertGoalStepsPage/insertGoalSteps.page';
 import ListGoals from '../../components/ListGoals/listGoals.component';
 import SetGoalDurationPage from '../SetGoalDurationPage/setGoalDurationPage.page';
 
 function MainPage() {
   return (
-    <Sequence
-      data={[
-        <div>
-          <InsertGoal />
-          <ListGoals />
-        </div>,
-        <SetGoalDurationPage />,
-      ]}
-    />
+    <div>
+      <Sequence
+        data={[
+          <div>
+            <InsertGoal />
+            <ListGoals />
+          </div>,
+          <SetGoalDurationPage />,
+          <InsertGoalStepsPage />,
+        ]}
+      />
+    </div>
   );
 }
 
