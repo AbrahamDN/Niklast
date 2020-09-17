d3b87a00be62c89b046ec7d3bbc6bbbef221a8b1
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-17 15:03:16 +0100

    [NKL-15] Updated TodaysSteps page to show progression

--- src/pages/TodaysSteps/todaysSteps.page.jsx
+++ src/pages/TodaysSteps/todaysSteps.page.jsx
@@ -5,51 +5,53 @@ import { useLocalStorage } from '../../Hooks/useLocalStorage';
 
 import ListSteps from '../../components/ListSteps/listSteps.component';
 
+function StepsProgress({ steps, progress }) {
+  const {
+    goalsState: [goals],
+  } = useContext(StoreContext);
+  const countSteps = goals.map(goal => (goal.steps ? goal.steps.length : 0));
+  const totalSteps =
+    countSteps && countSteps.length
+      ? countSteps.reduce((acc, curr) => acc + curr)
+      : null;
+  const getCompletedSteps = steps.map(step => (step.complete ? step : 0));
+  const completedSteps = getCompletedSteps.filter(array => array).length;
+
+  if (progress && progress === 'alone') {
+    return <progress value={completedSteps} max={totalSteps}></progress>;
+  }
+
+  if (progress && progress === 'none') {
+    return <p>{`${completedSteps} / ${totalSteps}`}</p>;
+  }
+
+  return (
+    <div>
+      <p>{`${completedSteps} / ${totalSteps}`}</p>
+      <progress value={completedSteps} max={totalSteps}></progress>
+    </div>
+  );
+}
+
 function TodaysSteps() {
   const {
     goalsState: [goals],
-    shuffle,
   } = useContext(StoreContext);
-  const [shuffled, setShuffled] = useLocalStorage('shuffled', false);
-  const [shuffledGoals, setShuffledGoals] = useLocalStorage(
-    'shuffledGoals',
-    []
-  );
-
-  const handleShuffle = array => {
-    if (!shuffled) {
-      shuffle(array);
-      setShuffled(true);
-    }
-  };
-
-  React.useEffect(() => {
-    console.log(shuffled);
-    return setShuffledGoals(goals);
-  }, []);
 
   return (
     <div>
       <h1>Steps to take today</h1>
-      {shuffled && shuffledGoals.length
-        ? shuffledGoals.map(goal => {
-            return (
-              <div key={goal.id}>
-                {handleShuffle(goal.steps)}
-                <ListSteps steps={goal.steps} />
-              </div>
-            );
-          })
-        : goals.map(goal => {
-            return (
-              <div key={goal.id}>
-                {handleShuffle(goal.steps)}
-                <ListSteps steps={goal.steps} />
-              </div>
-            );
-          })}
+      {goals.map(goal => {
+        return (
+          <div key={goal.id}>
+            <ListSteps steps={goal.steps} goal={goal} />
+          </div>
+        );
+      })}
 
-      <p>1/5</p>
+      {goals.map(goal =>
+        goal.steps ? <StepsProgress key={goal.id} steps={goal.steps} /> : null
+      )}
 
       <Link to=''>
         <button>Check Progression</button>
