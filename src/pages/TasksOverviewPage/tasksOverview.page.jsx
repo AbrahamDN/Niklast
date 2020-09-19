dbf0ddeb540d00efeb8b3233afc0c3a2fe81cf0c
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-19 23:59:31 +0100

    [NKL-14] Removed map method & shortened the code

--- src/pages/TasksOverviewPage/tasksOverview.page.jsx
+++ src/pages/TasksOverviewPage/tasksOverview.page.jsx
@@ -1,31 +1,14 @@
-import React, { useContext } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { StoreContext } from '../../context/storeContext';
-import ListSteps from '../../components/ListSteps/listSteps.component';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import ListGoals from '../../components/ListGoals/listGoals.component';
 
 function TasksOverview() {
-  const {
-    goalsState: [goals],
-  } = useContext(StoreContext);
   const history = useHistory();
 
   return (
     <div>
       <h1>Tasks Overview</h1>
-      {goals.length ? (
-        goals.map(goal => (
-          <ListGoals key={goal.id} selectGoal>
-            <ListSteps steps={goal.steps} max={3} valueOnly />
-          </ListGoals>
-        ))
-      ) : (
-        <div>
-          <h2>You have not set any goals</h2>
-          <Link to='/'>Add goals</Link>
-        </div>
-      )}
-
+      <ListGoals steps selectGoal />
       <div>
         <button onClick={() => history.push('/')}>Add a new task</button>
         <button onClick={() => history.push('/tasks-today')}>
