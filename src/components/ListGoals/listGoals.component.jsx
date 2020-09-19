81d961af3cad1c2a358e994f6a35d72760fcb81b
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-19 02:06:43 +0100

    [NKL-2] Updated ListGoals

--- src/components/ListGoals/listGoals.component.jsx
+++ src/components/ListGoals/listGoals.component.jsx
@@ -1,17 +1,26 @@
 import React, { useContext } from 'react';
 import { StoreContext } from '../../context/storeContext';
 
-function ListGoals({ ...otherProps }) {
+function ListGoals({ onClickSetSelectedGoal, ...otherProps }) {
   const {
     goalsState: [goals],
+    selectedGoalState,
   } = useContext(StoreContext);
+  const setSelectedGoal = selectedGoalState[1];
+
   return (
     <div {...otherProps}>
       <div>
         <ul>
           {goals
             ? goals.map((goal, key) => (
-                <li key={goal.id || key}>{goal.goal}</li>
+                <li
+                  onClick={() =>
+                    onClickSetSelectedGoal ? setSelectedGoal(goal) : null
+                  }
+                  key={goal.id || key}>
+                  {goal.goal}
+                </li>
               ))
             : null}
         </ul>
