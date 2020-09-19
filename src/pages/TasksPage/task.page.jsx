3dbde4c184c983ac426e7bfad38582c61fc950cc
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-19 01:39:05 +0100

    [NKL-17] Updated Task page

--- src/pages/TasksPage/task.page.jsx
+++ src/pages/TasksPage/task.page.jsx
@@ -6,13 +6,18 @@ import Progress from '../../components/Progess/progress.component';
 
 function TaskPage() {
   const {
+    goalsState: [goals],
     chosenGoalState: [chosenGoal],
   } = useContext(StoreContext);
 
   return (
     <div>
       <h1>Task</h1>
-      <ListSteps steps={chosenGoal.steps} />
+      {goals.map(goal =>
+        goal.id === chosenGoal.id ? (
+          <ListSteps key={goal.id} steps={goal.steps} goal={goal} />
+        ) : null
+      )}
       <Progress data={chosenGoal.steps} />
     </div>
   );
