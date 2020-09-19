b270ce7ecfe71fb311becddf6c4308351bf9413d
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-19 02:08:36 +0100

    [NKL-16] Updated GoalStep isComplete function to setSelectedGoal

--- src/components/GoalStep/goalStep.component.jsx
+++ src/components/GoalStep/goalStep.component.jsx
@@ -1,34 +1,45 @@
 import React from 'react';
 import { StoreContext } from '../../context/storeContext';
 
-// import ListSteps from '../ListSteps/listSteps.component';
-
 function GoalStep({ step, goal, valueOnly, ...otherProps }) {
   const {
     goalsState: [goals, setGoals],
+    selectedGoalState: [selectedGoal, setSelectedGoal],
   } = React.useContext(StoreContext);
+  const [render, setRender] = React.useState(false);
 
-  const isComplete = event => {
-    const complete = event.target.checked;
+  const isComplete = ref => {
+    const complete = ref.checked;
+    const updateSteps = data => {
+      return {
+        ...data,
+        steps: data.steps.map(mapStep =>
+          mapStep === step ? { ...mapStep, complete: complete } : mapStep
+        ),
+      };
+    };
+
+    if (selectedGoal.id === goal.id) setSelectedGoal(updateSteps(goal));
     return setGoals(
-      goals.map(obj =>
-        obj.id === goal.id
-          ? {
-              ...obj,
-              steps: obj.steps.map(mapStep =>
-                mapStep === step ? { ...mapStep, complete: complete } : mapStep
-              ),
-            }
-          : obj
-      )
+      goals.map(obj => {
+        return obj.id === goal.id ? updateSteps(obj) : obj;
+      })
     );
   };
+
+  React.useEffect(() => {
+    // setInterval is there to make the jsx wait for the data before rending
+    //This fixed the problem of outdated data  in jsx when switching between ages
+    setInterval(() => {
+      setRender(true);
+    }, 100);
+  }, []);
 
   if (valueOnly) {
     return <span {...otherProps}>{step.value}</span>;
   }
 
-  return (
+  return render ? (
     <div {...otherProps}>
       {goal ? (
         <cite>
@@ -36,12 +47,15 @@ function GoalStep({ step, goal, valueOnly, ...otherProps }) {
         </cite>
       ) : null}
       <div>{step.value}</div>
+
       <input
         type='checkbox'
         defaultChecked={step.complete || false}
-        onChange={e => isComplete(e)}
+        onChange={e => isComplete(e.target)}
       />
     </div>
+  ) : (
+    'Loading..'
   );
 }
 
