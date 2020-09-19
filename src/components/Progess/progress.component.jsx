b1b7d0a4cfd3aaf9c06623540246233cffe55bbf
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-19 03:25:23 +0100

    [NKL-18] Updated Progress to return something if no data

--- src/components/Progess/progress.component.jsx
+++ src/components/Progess/progress.component.jsx
@@ -4,30 +4,35 @@ function Progress({ data, onlyBar, noBar, ...otherProps }) {
   let total = [];
   let completed = [];
 
-  data.map(result => {
-    function handler() {
-      if (result.steps) {
-        for (let i = 0; i < result.steps.length; i++) {
-          const obj = result.steps[i];
-          if (obj.complete) completed.push(obj.complete);
-          return total.push(obj);
-        }
-      }
+  if (!data) return <pre>No Steps</pre>;
 
-      if (result.value) {
+  if (data)
+    data.map(result => {
+      function handler() {
+        if (result.steps) {
+          for (let i = 0; i < result.steps.length; i++) {
+            const obj = result.steps[i];
+            if (obj.complete) completed.push(obj.complete);
+            return total.push(obj);
+          }
+        }
+
+        if (result.value) {
+          if (result.complete) completed.push(result);
+          return total.push(result);
+        }
+
         if (result.complete) completed.push(result);
+        else
+          console.warn(
+            `Progress: could not find 'complete' property in object.`
+          );
+
         return total.push(result);
       }
 
-      if (result.complete) completed.push(result);
-      else
-        console.warn(`Progress: could not find 'complete' property in object.`);
-
-      return total.push(result);
-    }
-
-    return handler();
-  });
+      return handler();
+    });
 
   total = total.length;
   completed = completed.length;
