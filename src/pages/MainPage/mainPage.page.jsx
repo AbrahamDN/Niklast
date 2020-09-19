b49a87c0ec2fd0fa168aedb46951763073fb31b5
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-19 03:26:21 +0100

    [NKL-19] Added selectGoal prop to LiastGoals in main pg

--- src/pages/MainPage/mainPage.page.jsx
+++ src/pages/MainPage/mainPage.page.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 import Sequence from '../../components/Sequence/sequence.component';
 import InsertGoal from '../../components/InsertGoal/insertGoal.component';
 import InsertGoalStepsPage from '../InsertGoalStepsPage/insertGoalSteps.page';
 import ListGoals from '../../components/ListGoals/listGoals.component';
 import SetGoalDurationPage from '../SetGoalDurationPage/setGoalDurationPage.page';
-import TaskPage from '../TasksPage/task.page';
 
 function MainPage() {
   return (
@@ -15,13 +13,12 @@ function MainPage() {
         data={[
           <div>
             <InsertGoal />
-            <ListGoals />
+            <ListGoals selectGoal />
           </div>,
           <SetGoalDurationPage />,
           <InsertGoalStepsPage />,
         ]}
       />
-      <TaskPage />
     </div>
   );
 }
