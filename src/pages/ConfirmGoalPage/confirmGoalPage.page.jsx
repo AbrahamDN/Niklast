59729d99b267520e1340486a7a3bef8ed25ae643
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-09-19 15:46:49 -0400

    [NKL-12] Added steps list to confirmPage component

--- src/pages/ConfirmGoalPage/confirmGoalPage.page.jsx
+++ src/pages/ConfirmGoalPage/confirmGoalPage.page.jsx
@@ -1,13 +1,17 @@
 import React, { useContext, useState } from 'react';
+import ListSteps from '../../components/ListSteps/listSteps.component';
 import SequenceNextBtn from '../../components/Sequence/sequenceNextBtn.component';
 import { StoreContext } from '../../context/storeContext';
 
 function ConfirmGoalPage(){
     const {currentGoalState: [currentGoal],} = useContext(StoreContext);
+    const steps = currentGoal.steps;
+    
     return(
         <div>
             <h1>Here’s your plan to completeing this goal</h1>
             <h2>{currentGoal.goal}</h2>
+            {steps ? <ListSteps steps={steps} valueOnly /> : null}
 
             <p>Looks Good?</p>
             <SequenceNextBtn />
