128edbd84c14310a0131953622a5c0bd5e617b24
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-20 06:10:48 +0100

    [NKL-13] Updated ListSteps props (cite, withDate)

--- src/components/ListSteps/listSteps.component.jsx
+++ src/components/ListSteps/listSteps.component.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 import GoalStep from '../GoalStep/goalStep.component';
 // import Progress from '../Progess/progress.component';
 
-function ListSteps({ steps, max, goal, valueOnly, ...otherProps }) {
+function ListSteps({
+  steps,
+  max,
+  goal,
+  cite,
+  withDate,
+  valueOnly,
+  ...otherProps
+}) {
   if (max && !(typeof max === 'number'))
     console.warn(`ListSteps:`, ` typeof 'max' prop must be a number`);
 
-  if (!steps) {
-    return (
-      <span>
-        {console.warn(
-          `ListSteps: 'steps' prop is missing / invalid`,
-          `Either the prop is missing or the data cannot be mapped`,
-          { steps }
-        )}
-      </span>
-    );
-  }
+  if (!steps) return null;
 
   steps = steps && max && typeof max === 'number' ? steps.slice(0, max) : steps;
 
@@ -25,9 +23,14 @@ function ListSteps({ steps, max, goal, valueOnly, ...otherProps }) {
       {steps.map((step, key) => (
         <li {...otherProps} key={key}>
           {valueOnly ? (
-            <GoalStep step={step} valueOnly />
+            <GoalStep step={step} valueOnly withDate={withDate} />
           ) : (
-            <GoalStep step={step} goal={goal ? goal : null} />
+            <GoalStep
+              step={step}
+              goal={goal ? goal : null}
+              cite={cite}
+              withDate={withDate}
+            />
           )}
         </li>
       ))}
