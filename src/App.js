1a89154840c502819ac9810422686c8cb148cae2
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-09-22 12:47:53 -0400

    [Master] Added navigation component to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import Navigation from './components/Navigation/navigation.component';
 
 import MainPage from './pages/MainPage/mainPage.page';
 import TasksOverview from './pages/TasksOverviewPage/tasksOverview.page';
@@ -11,6 +12,7 @@ import TodaysSteps from './pages/TodaysSteps/todaysSteps.page';
 function App() {
   return (
     <div className='App'>
+      <Navigation/>
       <Switch>
         <Route path='/task' component={TaskPage} exact />
         <Route path='/tasks' component={TasksOverview} exact />
